787b946ef3912f324b6e273a78434185ac784a47
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-02-07 17:44:10 +0100

    First parser

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,15 @@
+public class Building {
+    public int nbFloorsAboveGround;
+
+    public Building(int nbFloorsAboveGround) {
+        this.nbFloorsAboveGround = nbFloorsAboveGround;
+    }
+
+    public int getnbFloorsAboveGround() {
+        return this.nbFloorsAboveGround;
+    }
+
+    public void setAddressType(int nbFloorsAboveGround) {
+        this.nbFloorsAboveGround = nbFloorsAboveGround;
+    }
+}
---
+++ Assets/Scripts/JSONBuildingParser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using Newtonsoft.Json.Linq;
+
+public class JSONBuildingParser
+{
+    static void Main(string[] args)
+    {
+        var BuildingString = File.ReadAllText("Building.json");
+        var Building = JObject.Parse(BuildingString);
+        var floorsAboveGround = Building.SelectToken("floorsAboveGround").Value<JObject>();
+        int nbFloorsAboveGround = floorsAboveGround.SelectToken("value").Value<int>();
+        Building building = new Building(nbFloorsAboveGround);
+    }
+}
