d6e2b3cd65791c73a2d83a3ef5d9c885ccbe1aaa
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-02-14 15:49:31 +0100

    Room parser added

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,35 @@
+public class Room {
+    private int numberOfDoors;
+    private int numberofWindows;
+    private int[][] coordinates;
+
+    public Room(int numberOfDoors, int numberofWindows, int[][] coordinates) {
+        this.numberOfDoors = numberOfDoors;
+        this.numberofWindows = numberofWindows;
+        this.coordinates = coordinates;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public int getNumberOfWindows() {
+        return numberofWindows;
+    }
+
+    public int[][] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void setNumberOfWindows(int numberofWindows) {
+        this.numberofWindows = numberofWindows;
+    }
+
+    public void setCoordinates(int[][] coordinates) {
+        this.coordinates = coordinates;
+    }
+}
