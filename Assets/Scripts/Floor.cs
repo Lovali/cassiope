88fc15773648109b0b7f73d87ce1d9d9094508be
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-02-21 17:05:53 +0100

    Floor parser finished

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -1,3 +1,55 @@
 public class Floor {
-    
+
+    private int numberOfRooms;
+    private int[] roomsOnFloor;
+    private int[][] coordinates;
+    private int height;
+
+    public Floor(int numberOfRooms, int[] roomsOnFloor, int[][] coordinates, int height)
+    {
+        this.numberOfRooms = numberOfRooms;
+        this.roomsOnFloor = roomsOnFloor;
+        this.coordinates = coordinates;
+        this.height = height;
+    }
+
+    public int getNumberOfRooms()
+    {
+        return numberOfRooms;
+    }
+
+    public int[] getRoomsOnFloor()
+    {
+        return roomsOnFloor;
+    }
+
+    public int[][] getCoordinates()
+    {
+        return coordinates;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms)
+    {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public void setRoomsOnFloor(int[] roomsOnFloor)
+    {
+        this.roomsOnFloor = roomsOnFloor;
+    }
+
+    public void setCoordinates(int[][] coordinates)
+    {
+        this.coordinates = coordinates;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
 }
--- Assets/Scripts/JSONFloorParser.cs
+++ Assets/Scripts/JSONFloorParser.cs
@@ -8,11 +8,17 @@ public class JSONFloorParser {
 
     static void Main(string[] args)
     {
-        var FloorString = File.ReadAllText(args[1]);
+        var FloorString = File.ReadAllText("Floor1.json");
         var Floor = JObject.Parse(FloorString);
+        var nbOfRooms = Floor.SelectToken("numberOfRooms").Value<JObject>();
+        int numberOfRooms = nbOfRooms.SelectToken("value").Value<int>();
         var roomsOnFloor = Floor.SelectToken("roomsOnFloor").Value<JObject>();
-        int numberOfRooms = Floor.SelectToken("numberOfRooms").Value<int>();
-        
-        Building building = new Building(nbFloorsAboveGround);
+        int[] objRoomsOnFloor = roomsOnFloor.SelectToken("object").Value<int[]>();
+        var location = Floor.SelectToken("location").Value<JObject>();
+        var locationValue = location.SelectToken("value").Value<JObject>();
+        int[][] coordinates = locationValue.SelectToken("coordinates").Value<int[][]>();
+        int height = locationValue.SelectToken("height").Value<int>();
+
+        Floor floor = new Floor(numberOfRooms, objRoomsOnFloor, coordinates, height);
     }
 }
