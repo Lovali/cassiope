f2f00255efe1db29d8077be8b5ec855b33ecaa9e
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-02-21 16:27:41 +0100

    Beginning of the modelisation

---
+++ Assets/Scripts/BuildingCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using Newtonsoft.Json.Linq;
+
+public class BuildingCreator : MonoBehaviour
+{
+    [SerializeField] GameObject wall;
+    [SerializeField] GameObject floor;
+
+    private void Start()
+    {
+        Room room = roomParser();
+
+    }
+
+    public Room roomParser()
+    {
+        var RoomString = File.ReadAllText("Room1.json");
+        var Room = JObject.Parse(RoomString);
+        var doors = Room.SelectToken("numbersOfDoors").Value<JObject>();
+        int nbDoors = doors.SelectToken("value").Value<int>();
+        var windows = Room.SelectToken("numberOfWindows").Value<JObject>();
+        int nbWindows = windows.SelectToken("value").Value<int>();
+        var location = Room.SelectToken("location").Value<JObject>();
+        var locationValue = location.SelectToken("value").Value<JObject>();
+        int[][] coordinates = locationValue.SelectToken("coordinates").Value<int[][]>();
+        Room room = new Room(nbDoors, nbWindows, coordinates);
+        return room;
+    }
+}
