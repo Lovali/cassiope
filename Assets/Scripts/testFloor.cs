46a0d9ea80e110edd6677dcb1c04b9469d5a8a06
Solene <suchuku35@gmail.com>
2022-03-07 15:26:15 +0100

    Trying a plug in to parse json

---
+++ Assets/Scripts/testFloor.cs
@@ -0,0 +1,13 @@
+﻿
+public class testFloor {
+    public string id;
+    public string type;
+    public string[] nameFloor;
+    public string[] numberOfRoom;
+    public string[] roomsOnFloor;
+    public string[] withinBuilding;
+    public string[][][] location;
+    public string context;
+
+
+}
---
+++ Assets/Scripts/testFloorCreator.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testFloorCreator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        testFloor floor = JsonConvert.DeserializeObject<testFloor>("Room1.json");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
