4abe8ef567decabd645cda0b6d7b243091f97443
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-03-23 20:04:59 +0100

    Scripts to call an API added

---
+++ Assets/Scripts/jsonController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class jsonController : MonoBehaviour
+{
+    public string jsonURL;
+
+    void Start()
+    {
+        StartCoroutine(getData());    
+    }
+
+    IEnumerator getData(){
+        WWW _www = new WWW(jsonURL);
+        yield return _www;
+
+        if(_www.error == null){
+            processJsonData(_www.text);
+        } else {
+            UnityEngine.Debug.Log("Something went wrong");
+        }
+    }
+
+    private void processJsonData(string _url){
+        jsonDataClass jsnData = JsonUtility.FromJson<jsonDataClass>(_url);
+        UnityEngine.Debug.Log(jsnData.playerName);
+        UnityEngine.Debug.Log(jsnData.balls);
+    }
+}
