aea68b1cb82871418f0cc471f878da9015a54525
Solene <suchuku35@gmail.com>
2022-05-17 18:50:12 +0200

    Add from server part

--- Assets/Scripts/jsonController.cs
+++ Assets/Scripts/jsonController.cs
@@ -10,23 +10,34 @@ public class jsonController : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(getData());    
+        StartCoroutine(getData());
     }
 
-    IEnumerator getData(){
+    IEnumerator getData()
+    {
         WWW _www = new WWW(jsonURL);
         yield return _www;
 
-        if(_www.error == null){
+        if (_www.error == null)
+        {
             processJsonData(_www.text);
-        } else {
+        }
+        else
+        {
             UnityEngine.Debug.Log("Something went wrong");
         }
     }
 
-    private void processJsonData(string _url){
-        jsonDataClass jsnData = JsonUtility.FromJson<jsonDataClass>(_url);
-        UnityEngine.Debug.Log(jsnData.playerName);
-        UnityEngine.Debug.Log(jsnData.balls);
+    private void processJsonData(string _url)
+    {
+        // Floor floor = JsonUtility.FromJson<Floor>(_url);
+        jsonRoom room = JsonUtility.FromJson<jsonRoom>(_url);
+        // jsonDataClass jsnData = JsonUtility.FromJson<jsonDataClass>(_url);
+        // UnityEngine.Debug.Log(jsnData.playerName);
+        // UnityEngine.Debug.Log(jsnData.balls[0].name);
+        // UnityEngine.Debug.Log(floor.getHeight());
+        UnityEngine.Debug.Log(room.id);
+        UnityEngine.Debug.Log(room.type);
+        UnityEngine.Debug.Log(room.Location.value.coordinates);
     }
 }
---
+++ Assets/Scripts/jsonRoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Collections;
+using Newtonsoft.Json;
+
+
+[Serializable]
+public class jsonRoom
+{
+    public string id;
+    public string type;
+    public location Location;
+
+    [Serializable]
+    public class location
+    {
+        public string type;
+        public Value value;
+        public double[][][] coordinates;
+    }
+
+    [Serializable]
+    public class Value
+    {
+        public string type;
+        public double[][][] coordinates;
+    }
+
+
+}
